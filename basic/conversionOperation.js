b0002668359b271a9152edce3eb76bba682a6a12
Aditi Singh <aditidhn06@gmail.com>
2024-04-02 22:09:47 +0530

    Operations of dataType

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -29,5 +29,28 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 
 let someNumber=44;
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************Operations*********************************
+let value =3;
+let negvalue=-value;
+// console.log(negvalue);
+
+let str1="hii";
+let str2="  world";
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log("1"+2+2); 
+// console.log(1+2+"2");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+// ++gameCounter; //prefix=increment operator increments & reture the value after incrementing,++x
+gameCounter++; //postfix=increment operator increments & reture the value before incrementing,x++
+console.log(gameCounter);
