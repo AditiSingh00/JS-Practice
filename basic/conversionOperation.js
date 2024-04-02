45c878284eed23a7f035b2d10d1072e411da66fc
Aditi Singh <aditidhn06@gmail.com>
2024-04-02 21:47:43 +0530

    conversionOperation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,33 @@
+// let score="44bb";
+// let score="44";
+// let score =null;
+// let score=undefined;
+// let score=true;
+let score="Adi";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"44" =>33
+//"44bb"=> NaN
+//true=>1;false=>0
+
+// let isLoggedIn= 1
+// let isLoggedIn= " ";
+// let isLoggedIn= "";
+let isLoggedIn= "Adi";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+//""=> false
+//"Adi"=>true
+
+let someNumber=44;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
