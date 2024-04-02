6b38b91e4752342f1c462e94c5dc50cf9edabc67
Aditi Singh <aditidhn06@gmail.com>
2024-04-02 20:07:18 +0530

    dataTypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+// alert('hello');  we are using nodejs,not browser
+
+let name="Adi"; // string =>" "
+let age=23;  // numbers 
+let isLoggedin=false; // boolean => true/false 
+
+// number =>2 to power 53 
+// bigint  
+// null =>standalone value 
+// undefines 
+// symbol => unique
+// object
+
+console.log(typeof "Adi");
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
