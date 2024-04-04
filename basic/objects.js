df528c5291d1a6ac3cab14d14b763d058ea8446d
Aditi Singh <aditidhn06@gmail.com>
2024-04-04 21:01:47 +0530

    more in objects

--- basic/objects.js
+++ basic/objects.js
@@ -1,7 +1,8 @@
 //Singleton
 //Object.create >>> //constructor method
 
-//************object literals**********8
+
+//************object literals**********///
 
 const mySym =Symbol("key1")
 
@@ -32,8 +33,65 @@ newUser.greeting= function(){
 newUser.greeting1= function(){
     console.log(`heelloo , ${this.name}`);
 }
-console.log(newUser.greeting);
-console.log(newUser.greeting());
-console.log(newUser.greeting1());
+// console.log(newUser.greeting);
+// console.log(newUser.greeting());
+// console.log(newUser.greeting1());
 
+
+//**************************More Object************//
+
+// const tinderUser =new Object() // singleton object
+const tinderUser={}  //Non-Sigleton Object
+tinderUser.id="abc1";
+tinderUser.name="adi";
+tinderUser.isLoggedIn=false;
+// console.log(tinderUser);
+
+const reularUser ={
+    mail:"abc@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"adiii",
+            lastname:"singh",
+        }
+    }
+}
+// console.log(reularUser.fullname.userfullname.firstname);
+
+const obj1={
+    1:"a",
+    2:"b"
+}
+const obj2={
+    1:"a",
+    2:"b"
+}
+// const obj3={obj1,obj2}
+// const obj3=Object.assign(obj1,obj2)
+// const obj4=Object.assign({},obj1,obj2)  // {} = optional parameters
+const obj3={...obj1,...obj2}
+// console.log(obj3);
+// console.log(obj4);
+
+const users=[
+    {
+        id:1,
+        name:"adi",
+    },
+    {
+        id:2,
+        name:"adiccc",
+    },
+    {
+        id:3,
+        name:"addddi",
+    }
+]
+users[1].name
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
 
