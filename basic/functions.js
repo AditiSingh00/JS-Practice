576070e2d522650a2e6a31f50d2381c18e6bc708
Aditi Singh <aditidhn06@gmail.com>
2024-04-07 23:04:21 +0530

    functions

---
+++ basic/functions.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+    console.log("hii");
+}
+
+// sayMyName  >>>this is reference
+// sayMyName( )     //this is execution
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+}
+addTwoNumbers(3,5)
+
+
