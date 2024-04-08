37b473af154ceb18a0a620690bc3d2cb8bdfec98
Aditi Singh <aditidhn06@gmail.com>
2024-04-08 23:21:18 +0530

    function

--- basic/functions.js
+++ basic/functions.js
@@ -6,10 +6,11 @@
 // sayMyName( )     //this is execution
 
 function addTwoNumbers(num1,num2){  // (num1,num2) >>parameters
-    let result = num1+num2
-    return result
+    // let result = num1+num2
+    // return result
+    return num1+num2
 }
-addTwoNumbers(3,5) //arguements
+// addTwoNumbers(3,5) //arguements
 const result = addTwoNumbers(3,5) //arguements
 console.log(result);
 
