4fd3497803cf70968567b433656166731d462d8c
Aditi Singh <aditidhn06@gmail.com>
2024-04-10 20:36:09 +0530

    function

--- basic/functions.js
+++ basic/functions.js
@@ -5,18 +5,23 @@
 // sayMyName  >>>this is reference
 // sayMyName( )     //this is execution
 
-function addTwoNumbers(num1,num2){  // (num1,num2) >>parameters
+function addTwoNumbers(num1,num2){  // (num1,num2) >>parameters>>>function ki definition
     // let result = num1+num2
     // return result
     return num1+num2
 }
-// addTwoNumbers(3,5) //arguements
+// addTwoNumbers(3,5) //arguements >>function ko call krna
 const result = addTwoNumbers(3,5) //arguements
-// console.log(result);
+// console.log(result);  //undefined
 
-function loginUser(user1){
+// function loginUser(user1){
+function loginUser(user1 ="Adii"){
+    // if(user1 === undefined){
+        if(!user1){
+       console.log("please enter a username");
+       return
+    }
     return`${user1} just log in`
 }
 console.log(loginUser("hii"));
 // console.log(loginUser());  undefined
-
